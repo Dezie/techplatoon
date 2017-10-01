0477f5c64cd79c686815cdfc82616d8d2a6e86c4
Charlette N'Guessan <charlette1693@gmail.com>
2017-10-01 17:57:40 +0000

    Update boardlist.py

--- boardlist.py
+++ boardlist.py
@@ -13,6 +13,20 @@ class BoardList:
 
     def view_tasks(self):
         pass
+class card(object):
+    """Class that matches a dict with card details from json api response."""
+
+    def __init__(self, name):
+        self.name = name
+
+    def __eq__(self, other):
+        if isinstance(other, dict):
+            return other.get("name", None) == self.name
+        return NotImplemented
+
+    def __repr__(self):
+        return "{}({!r}, {!r})".format(
+            self.__class__.__name__, self.key, self.value)
 
 
 if __name__ == '__main__':
